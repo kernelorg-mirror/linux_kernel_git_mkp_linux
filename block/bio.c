60f680e906004165cb02019096b839aebcc43be8
Martin K. Petersen <martin.petersen@oracle.com>
2015-04-21 20:06:10 -0400

    oracleasm: 4.0 compat changes
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- block/bio.c
+++ block/bio.c
@@ -1445,6 +1445,7 @@ struct bio *bio_map_user_iov(struct request_queue *q,
 	bio_put(bio);
 	return ERR_PTR(ret);
 }
+EXPORT_SYMBOL(bio_map_user_iov);
 
 static void __bio_unmap_user(struct bio *bio)
 {
